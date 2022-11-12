9812e89229a737e34323e581a34635e5d104311d
范华 <fanhua@laiye.com>
2022-11-12 17:12:43 +0800

    feat: add aosjs

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,12 @@
 /*
  * @Author: fanhua 
  * @Date: 2022-11-11 09:22:02 
- * @Last Modified by:   fanhua 
- * @Last Modified time: 2022-11-11 09:22:02 
+ * @Last Modified by: fanhua
+ * @Last Modified time: 2022-11-12 17:12:18
  */
 
+// require style file for build
 require('../source/style/less/index.less')
+
+// aos init
+AOS.init();
