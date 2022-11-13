b9020367c21c0f300957999b38c21b185eeb7e04
范华 <fanhua@laiye.com>
2022-11-13 15:32:46 +0800

    feat: add code highlight

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,3 +1,20 @@
+/**
+ * @description util function to cut string
+ * @param {string} string target string
+ * @param {number} length target index in string
+ */
 hexo.extend.helper.register('stringCut', (string, length) => {
   return string.slice(0, length);
+});
+/**
+ * @description util function to scroll top
+ * @param {string} cssSelect
+ */
+hexo.extend.helper.register('elementScrollToTop', (cssSelect, top=0, ) => {
+  const targetEL = document.querySelector(cssSelect) ?? null
+  if (targetEL) {
+    targetEL.scrollTo({
+
+    })
+  }
 });
